5015f1bd9330c0060f5461597cff5f57ebfca7e1
maharanianindya <anindyabudiarti@gmail.com>
2025-09-10 00:43:08 +0700

    assignment 2

--- main/views.py
+++ main/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render
 from .models import Product
 # Create your views here.
 def show_main(request):
-    products = Product.objects.all()
     context = {
         'app_name' : 'Queen Kicks Store',
         'name': 'Maharani Anindya Budiarti',
         'class': 'KKI',
-        'products': products,
+
     }
     return render(request, "main.html", context)
