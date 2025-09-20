263ecd90138495857b0603fa6848f525a6c54f34
maharanianindya <anindyabudiarti@gmail.com>
2025-09-20 18:11:01 +0700

    assignment 4

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Product(models.Model):
@@ -8,6 +9,7 @@ class Product(models.Model):
     thumbnail = models.URLField(blank=True, null=True)
     category = models.CharField()
     is_featured = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from main.views import show_main, show_xml, show_json, show_xml_by_id, show_json_by_id
 from main.views import add_product, product_detail
+from main.views import register
+from main.views import login_user
+from main.views import logout_user
 
 app_name = 'main'
 
@@ -12,4 +15,7 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('xml/<str:prod_id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<str:prod_id>/', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
